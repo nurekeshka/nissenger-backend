edbba55dee016c652aefdd47955d32ac5dc9fe0b
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-27 13:03:50 +0600

    feat: add test for invalid args in model

--- apps/timetable/tests.py
+++ apps/timetable/tests.py
@@ -7,4 +7,13 @@ from django.urls import reverse
 class ClassTestCase(APITestCase):
     def test_model_valid(self):
         for n in range(7, 13):
-            cl = Class.objects.create(grade=n, letter='A')
+            Class.objects.create(grade=n, letter='A')
+    
+    def test_model_invalid(self):
+        try:
+            Class.objects.create(grade=1, letter='A')
+            error = False
+        except:
+            error = True
+        
+        self.assertEqual(error, True)
