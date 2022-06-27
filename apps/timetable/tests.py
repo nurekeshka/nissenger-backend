e135f25daed60f6ff0f685b5cf9bdada3ce3e93a
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-27 13:22:39 +0600

    fix: better syntax for invalid model validation

--- apps/timetable/tests.py
+++ apps/timetable/tests.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase
+from apps.timetable.exceptions import *
 from apps.timetable.models import *
 from rest_framework import status
 from django.urls import reverse
@@ -7,13 +8,8 @@ from django.urls import reverse
 class ClassTestCase(APITestCase):
     def test_model_valid(self):
         for n in range(7, 13):
-            Class.objects.create(grade=n, letter='A')
+            Class.objects.create(grade=n, letter='A').save()
     
     def test_model_invalid(self):
-        try:
-            Class.objects.create(grade=1, letter='A')
-            error = False
-        except:
-            error = True
-        
-        self.assertEqual(error, True)
+        with self.assertRaises(ValidationError):
+            Class.objects.create(grade=1, letter='A').save()
