24d88f59a5cef5d0f9ed43ed6128bbce35816809
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-27 15:08:56 +0600

    fix: timetable without download date

--- apps/timetable/admin.py
+++ apps/timetable/admin.py
@@ -5,7 +5,7 @@ from .models import *
 @admin.register(Timetable)
 class TimetableAdmin(admin.ModelAdmin):
     list_display = ('id', 'download_date', 'publication_date')
-    fields = ('download_date', 'publication_date')
+    fields = ('publication_date',)
 
 
 @admin.register(Day)
