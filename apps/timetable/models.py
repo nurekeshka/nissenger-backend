00b528f79a1245b16bb1478f78a9b542c0e2ff8b
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-27 13:49:21 +0600

    fix: format

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 
 class Day(models.Model):
-    name = models.CharField(verbose_name='name', max_length=15, blank=False, null=False)
+    name = models.CharField(verbose_name='name',
+                            max_length=15, blank=False, null=False)
 
     class Meta:
         verbose_name = 'day'
@@ -16,17 +17,21 @@ class Day(models.Model):
 
 
 class Class(models.Model):
-    grade = models.IntegerField(verbose_name='grade', blank=False, null=False, validators=[
-        MaxValueValidator(limit_value=12),
-        MinValueValidator(limit_value=7),
-    ])
-    letter = models.CharField(verbose_name='letter', max_length=1, blank=False, null=False, validators=[
-        validate_class_letter
-    ])
+    grade = models.IntegerField(verbose_name='grade', blank=False, 
+        null=False, validators=[
+            MaxValueValidator(limit_value=12),
+            MinValueValidator(limit_value=7),
+        ]
+    )
+    letter = models.CharField(verbose_name='letter', max_length=1, 
+        blank=False, null=False, validators=[
+            validate_class_letter
+        ]
+    )
 
     class Meta:
         verbose_name = 'class'
         verbose_name_plural = 'classes'
-    
+
     def __str__(self):
         return f'{self.grade}{self.letter}'
