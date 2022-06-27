8e18c051be01168fdb1f8ceabd5603fb6068f773
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-27 13:11:34 +0600

    feat: add validator params

--- apps/timetable/exceptions.py
+++ apps/timetable/exceptions.py
@@ -3,4 +3,4 @@ from django.core.exceptions import ValidationError
 
 def validate_class_grade(value: int) -> None:
     if value not in range(7, 13):
-        raise ValidationError('Invalid class grade: %s' % value)
+        raise ValidationError('Invalid class grade: %s' % value, params={'value': value})
