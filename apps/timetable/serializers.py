e25f410111c79fa0977bb60c98ed430e4fa08175
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-23 00:39:12 +0600

    feat: add day serializer

--- apps/timetable/serializers.py
+++ apps/timetable/serializers.py
@@ -6,3 +6,9 @@ class TimetableSerializer(ModelSerializer):
     class Meta:
         model = models.Timetable
         fields = ('id', 'download_date', 'publication_date')
+
+
+class DaySerializer(ModelSerializer):
+    class Meta:
+        model = models.Day
+        fields = ('id', 'name')
