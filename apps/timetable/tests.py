2ac31194b89de9d88c6fd64f75634f1ffc2e6873
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-26 21:00:30 +0600

    del: no need in tests

--- apps/timetable/tests.py
+++ apps/timetable/tests.py
@@ -1,17 +1,10 @@
 from django.core.exceptions import ValidationError
 from rest_framework.test import APITestCase
-from apps.timetable.models import *
+from apps.timetable import models
 from rest_framework import status
 from django.urls import reverse
 
 
 class ClassTestCase(APITestCase):
-    def test_model_valid(self):
-        for n in range(7, 13):
-            Class.objects.create(grade=n, letter='A').full_clean()
-    
-    def test_model_invalid(self):
-        with self.assertRaises(ValidationError):
-            Class.objects.create(grade=1, letter='A').full_clean()
-        with self.assertRaises(ValidationError):
-            Class.objects.create(grade=11, letter='1').full_clean()
+    def test():
+        pass
