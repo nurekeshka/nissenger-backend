187b5fef661af16b968b4dc32875cfd128eb7a20
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-27 14:42:30 +0600

    feat: add load subjects

--- apps/timetable/management/commands/_parser.py
+++ apps/timetable/management/commands/_parser.py
@@ -18,7 +18,17 @@ def load_teachers(teachers: list, timetable: models.Timetable):
         models.Teacher.objects.create( name=name.lower().title(), timetable=timetable )
 
 
+def load_subjects(subjects: list, timetable: models.Timetable):
+    change = const.SUBJECT_CHANGES
+    changes = const.SUBJECT_CHANGES.keys()
+
+    for subject in subjects:
+        name = change[ subject['name'] ] if subject['name'] in changes else subject['name']
+        models.Subject.objects.create( name=name, timetable=timetable )
+
+
 def load_entities(tables: dict) -> dict:
     timetable = models.Timetable.objects.create()
 
     load_teachers(tables[indexes.teachers.value]['data_rows'], timetable)
+    load_subjects(tables[indexes.subjects.value]['data_rows'], timetable)
