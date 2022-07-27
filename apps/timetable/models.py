cea4b45c2ef2f388c35a6222038b4f015ecc648d
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-27 13:58:15 +0600

    fix: auto now add in timetable

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class Timetable(models.Model):
-    download_date = models.DateTimeField(default=datetime.now, verbose_name='download date')
+    download_date = models.DateTimeField(auto_now_add=True, verbose_name='download date')
     publication_date = models.DateTimeField(null=True, blank=True, verbose_name='publication date')
 
     class Meta:
