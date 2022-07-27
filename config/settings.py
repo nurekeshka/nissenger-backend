88cb25f37d5e81242ec21e9749c5085c528f80f8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-27 22:07:17 +0600

    feat: add telegram app

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Project apps
     'apps.accounts.apps.AccountsConfig',
     'apps.timetable.apps.TimetableConfig',
+    'apps.telegram.apps.TelegramConfig',
 ]
 
 MIDDLEWARE = [
