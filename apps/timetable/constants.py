60fad652898fa3cf5a3e2d70f184b8b7aab3f3c8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-27 13:08:29 +0600

    feat: add timetable main db url data

---
+++ apps/timetable/constants.py
@@ -0,0 +1,36 @@
+TIMETABLE_DATABASE_LINK = 'https://{}.edupage.org/rpr/server/maindbi.js?__func=mainDBIAccessor'
+TIMETABLE_DATABASE_DATA = {
+    "__args": [
+        None,
+        2021,
+        {},
+        {
+            "op": "fetch",
+            "needed_part": {
+                "teachers": [
+                    "short"
+                ],
+                "classes": [
+                    "short"
+                ],
+                "classrooms": [
+                    "short"
+                ],
+                "subjects": [
+                    "name"
+                ],
+                "periods": [
+                    "period",
+                    "starttime",
+                    "endtime"
+                ],
+                "dayparts": [
+                    "starttime",
+                    "endtime"
+                ]
+            },
+            "needed_combos": {}
+        }
+    ],
+    "__gsh": "00000000"
+}
