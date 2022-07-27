85197d9354aba9fafc42dcfeb7e4cc0fda642b5d
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-27 15:20:56 +0600

    feat: add load periods

--- apps/timetable/management/commands/_parser.py
+++ apps/timetable/management/commands/_parser.py
@@ -36,9 +36,15 @@ def load_offices(offices: list, timetable: models.Timetable):
         models.Office.objects.create( name=name, timetable=timetable )
 
 
+def load_periods(periods: list, timetable: models.Timetable):
+    for period in periods:
+        models.Period.objects.create( number=period['period'], start=period['starttime'], end=period['endtime'], timetable=timetable )
+
+
 def load_entities(tables: dict) -> dict:
     timetable = models.Timetable.objects.create()
 
-    load_teachers(tables[indexes.teachers.value]['data_rows'], timetable)
-    load_subjects(tables[indexes.subjects.value]['data_rows'], timetable)
-    load_offices(tables[indexes.classrooms.value]['data_rows'], timetable)
+    load_teachers( tables[indexes.teachers.value]['data_rows'], timetable )
+    load_subjects( tables[indexes.subjects.value]['data_rows'], timetable )
+    load_offices( tables[indexes.classrooms.value]['data_rows'], timetable )
+    load_periods( tables[indexes.periods.value]['data_rows'], timetable )
