47de9ffc2c86115689bf28329dcbe6376c8b2c0a
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-07 21:49:36 +0600

    feat: add school model

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -10,3 +10,16 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50, verbose_name='name')
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name='city')
+
+    class Meta:
+        verbose_name = 'school'
+        verbose_name_plural = 'schools'
+
+    def __str__(self):
+        return self.name
