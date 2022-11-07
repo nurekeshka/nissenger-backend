d530ad0493ce5b04aad51b3c79711de6c26b7d76
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-07 21:53:42 +0600

    feat: add timetable model

--- apps/timetable/models.py
+++ apps/timetable/models.py
@@ -23,3 +23,17 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Timetable(models.Model):
+    downloaded = models.DateTimeField(
+        auto_now_add=True, blank=True, verbose_name='downloaded')
+    school = models.ForeignKey(
+        School, on_delete=models.CASCADE, verbose_name='school')
+
+    class Meta:
+        verbose_name = 'timetable'
+        verbose_name_plural = 'timetables'
+
+    def __str__(self):
+        return self.downloaded.strftime('%Y-%m-%d %H:%M:%S')
