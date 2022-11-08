5c9c89ee3a132bb10b22dc496b8e374e8f7ce744
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-08 20:52:38 +0600

    fix: leave only days

--- apps/timetable/constants.py
+++ apps/timetable/constants.py
@@ -1,14 +1,3 @@
-from django.conf import settings
-from enum import Enum
-
-
-class TimetableIndexes(Enum):
-    teachers = 0
-    subjects = 1
-    classrooms = 2
-    classes = 3
-    periods = 4
-
 
 DAYS = (
     'monday',
