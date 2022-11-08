06fe6262ecea69712204b724e11530848fd5e350
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-08 20:52:25 +0600

    fix: remove exceptions

--- apps/timetable/exceptions.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_class_letter(value: str):
-    if not value.isalpha():
-        raise ValidationError('Invalid class letter: %s' % value, params={'class_letter': value})
