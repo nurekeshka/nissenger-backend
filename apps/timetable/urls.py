71082f929c1be24a85d66aa6cd96ee7a77c8dfb0
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-16 21:54:18 +0600

    del: school pk route

--- apps/timetable/urls.py
+++ apps/timetable/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('search-class/', views.SearchClass.as_view(), name='search-class'),
     path('upload', views.TimetableLoadView.as_view(), name='timetable-upload'),
     path('schools/', views.SchoolList.as_view(), name='list-schools'),
-    path('schools/<int:pk>/', views.RetrieveSchool.as_view(), name='get-school'),
     path('lessons/', views.LessonsList.as_view(), name='list-lessons'),
 ]
