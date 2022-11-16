3e2db9ddf36303da8c359ee56163d5ca4596ef80
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-16 20:25:27 +0600

    feat: add class not found exception

--- apps/timetable/exceptions.py
+++ apps/timetable/exceptions.py
@@ -5,3 +5,8 @@ from rest_framework import status
 class SchoolNotFoundException(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = 'School not found in the database.'
+
+
+class ClassNotFoundExceptions(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'Class not found in the database.'
