6d12c5c909364b743eee8656c97eb35cc72bdf27
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-22 12:46:42 +0600

    fix: rename search class route to get class

--- apps/timetable/urls.py
+++ apps/timetable/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search-class/', views.SearchClass.as_view(), name='search-class'),
+    path('get-class/', views.SearchClass.as_view(), name='get-class'),
     path('search-group/', views.SearchGroup.as_view(), name='search-group'),
     path('upload', views.TimetableLoadView.as_view(), name='timetable-upload'),
     path('schools/', views.SchoolList.as_view(), name='list-schools'),
