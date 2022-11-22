3f3fd3eb0cf9b70ca210d1681c76e4b27210a714
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-22 21:51:32 +0600

    feat: add city not found in database

--- apps/timetable/exceptions.py
+++ apps/timetable/exceptions.py
@@ -15,3 +15,8 @@ class ClassNotFoundException(APIException):
 class KeyErrorExceptionHandler(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Some arguments were not provided.'
+
+
+class CityNotFoundExceptionHandler(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'City not found in the database.'
