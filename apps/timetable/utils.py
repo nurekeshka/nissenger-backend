0c3ddecaf0c5a808529eb4053bfc2efe59c27496
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-22 13:10:25 +0600

    feat: add ordering by day and period function

--- apps/timetable/utils.py
+++ apps/timetable/utils.py
@@ -20,3 +20,18 @@ def search_for_group(name: str, classes: List[models.Class], timetable: models.T
 
     group = group.filter(count=len(classes))
     return group
+
+
+def sort_lessons_by_day_and_period(lessons_queryset: List[models.Lesson]) -> List[models.Lesson]:
+    days_to_lessons_dictionary = {day.name: list()
+                                  for day in models.Day.objects.all()}
+
+    for lesson in lessons_queryset:
+        days_to_lessons_dictionary[lesson.day.name].append(lesson)
+
+    output = list()
+
+    for day, lessons in days_to_lessons_dictionary.items():
+        output.extend(sorted(lessons, key=lambda lesson: lesson.period.number))
+
+    return output
