9e586d5410fc721855b9c9cf64d98ae437b628fc
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-22 13:11:10 +0600

    feat: add key error exception

--- apps/timetable/exceptions.py
+++ apps/timetable/exceptions.py
@@ -10,3 +10,8 @@ class TimetableNotFoundException(APIException):
 class ClassNotFoundException(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = 'Class not found in the database.'
+
+
+class KeyErrorExceptionHandler(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Some arguments were not provided.'
