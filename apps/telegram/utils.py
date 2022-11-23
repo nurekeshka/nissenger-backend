ad9f30e5f2696fd5a40599fcfb8247fd1c079c44
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-23 20:20:42 +0600

    feat: add admin chat id and forward method

--- apps/telegram/utils.py
+++ apps/telegram/utils.py
@@ -7,6 +7,7 @@ class TelegramBot(TeleBot):
     token = settings.TELEGRAM
     threaded = True
     parse_mode = 'html'
+    admin_chat_id = settings.TELEGRAM_ADMIN_CHAT
 
     def __init__(self):
         super(TelegramBot, self).__init__(
@@ -16,18 +17,24 @@ class TelegramBot(TeleBot):
         )
 
     def reply_to(self, message: types.Message, text: str, markup: types.InlineKeyboardMarkup = None):
-        bot.send_message(
+        self.send_message(
             chat_id=message.chat.id,
             text=text,
             reply_markup=markup,
         )
 
     def edit(self, message: types.Message, text: str, markup: types.InlineKeyboardButton = None):
-        bot.edit_message_text(
+        self.edit_message_text(
             chat_id=message.chat.id,
             message_id=message.id,
             text=text,
             reply_markup=markup,
+        )
+
+    def forward_to(self, chat_id: int, text: str):
+        self.send_message(
+            chat_id=chat_id,
+            text=text,
         )
 
 
