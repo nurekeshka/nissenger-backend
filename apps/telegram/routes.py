b360f24975109b1ad42fbccd7e7a66eb6a2c927e
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-23 22:18:23 +0600

    feat: add callback query handler

--- apps/telegram/routes.py
+++ apps/telegram/routes.py
@@ -4,7 +4,19 @@ from .utils import bot
 from . import views
 
 
+routepatterns = {
+    'write-report': views.ReportCallbackAction,
+    'forward_to_admins': views.ForwardToAdminChatCallbackAction,
+}
+
+
 @bot.message_handler(commands=['start'])
 def start_command(message: types.Message):
     bot.reply_to(message=message, text=MessageTexts.menu.value,
                  markup=views.MenuMarkup())
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query_handler(call: types.CallbackQuery):
+    view: views.BaseCallbackAction = routepatterns[call.data]()
+    view.callback_action(bot, call.message)
