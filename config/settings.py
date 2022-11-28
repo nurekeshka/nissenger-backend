ad7bbd227a1d25010ad893ce77050731605be1c8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-28 09:01:35 +0600

    fix[MAJOUR]: removing telegram, accounts functionality

--- config/settings.py
+++ config/settings.py
@@ -43,9 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     # Project apps
-    'apps.accounts.apps.AccountsConfig',
     'apps.timetable.apps.TimetableConfig',
-    'apps.telegram.apps.TelegramConfig',
 ]
 
 MIDDLEWARE = [
