a238647d059b9f1e29f81e1fa46e6088be89e773
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-12-05 21:26:24 +0600

    feat: add notify admins view

--- apps/telegram/views.py
+++ apps/telegram/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
+from apps.telegram.models import bot
+from rest_framework import views
+from apps.timetable import utils
 
-# Create your views here.
+
+class NotifyAdmins(views.APIView):
+    @utils.json
+    def post(self, request: Request, json: dict, *args, **kwargs):
+        message = json['message']
+        bot.send_to_admins(message)
+        return Response()
