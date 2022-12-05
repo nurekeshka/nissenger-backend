3fa85555f74560d4bc64128724398420dd839278
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-12-05 21:05:48 +0600

    feat: add telegram bot model

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from telebot import TeleBot
 
-# Create your models here.
+
+class TelegramBot(TeleBot):
+    token = settings.TELEGRAM_API_TOKEN
+    parse_mode = 'html'
+    admin_chat = settings.TELEGRAM_ADMIN_CHAT
+
+    def __init__(self):
+        super(TelegramBot, self).__init__(
+            token=self.token,
+            parse_mode=self.parse_mode,
+            threaded=True,
+        )
+
+    def send_to_admins(self, text: str):
+        self.send_message(
+            chat_id=self.admin_chat,
+            text=text,
+        )
+
+
+bot = TelegramBot()
