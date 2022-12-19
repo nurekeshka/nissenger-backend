1fc02f0b24ed4461a526358bd5954e39102d68d8
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-19 19:12:48 +0600

    feat: add parser chat & method to send there

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -7,12 +7,19 @@ class TelegramBot(TeleBot):
     token = settings.TELEGRAM_API_TOKEN
     parse_mode = 'html'
     admin_chat = settings.TELEGRAM_ADMIN_CHAT
+    parser_chat = settings.TELEGRAM_PARSER_CHAT
 
     def __init__(self):
         super(TelegramBot, self).__init__(
             token=self.token,
             parse_mode=self.parse_mode,
             threaded=True,
+        )
+
+    def send_to_parser(self, text: str):
+        self.send_message(
+            chat_id=self.parser_chat,
+            text=text,
         )
 
     def send_to_admins(self, text: str):
