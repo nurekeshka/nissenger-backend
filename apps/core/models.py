161ae40c8127402adb2adcb9ef9183fc4eae9583
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-21 18:42:16 +0600

    feat: add version model

---
+++ apps/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Version(models.Model):
+    major = models.PositiveSmallIntegerField(verbose_name='major')
+    minor = models.PositiveSmallIntegerField(verbose_name='minor')
+    patch = models.PositiveSmallIntegerField(verbose_name='patch')
+
+    class Meta:
+        verbose_name = 'version'
+        verbose_name_plural = 'versions'
+
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.patch}'
