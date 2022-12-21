38f5eba8d41406afad857195de70ba7e3a7fb2c7
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-21 18:42:24 +0600

    feat: add version admin

---
+++ apps/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Version)
+class VersionAdmin(admin.ModelAdmin):
+    fields = ('major', 'minor', 'patch')
+    list_display = ('major', 'minor', 'patch')
+    ordering = ('-major', '-minor', '-patch')
