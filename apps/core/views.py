6d83c0c207df7b1c122cae09782be17858e13666
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-21 19:11:24 +0600

    feat: add get latest and create version view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import generics
+from rest_framework import status
+from rest_framework import views
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+class GetLatestAndCreateVersion(generics.CreateAPIView):
+    serializer_class = serializers.VersionSerializer
+
+    def get(self, request, *args, **kwargs):
+        latest = models.Version.objects.all().order_by(
+            '-major', '-minor', '-patch').first()
+        serializer = serializers.VersionSerializer(instance=latest)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
