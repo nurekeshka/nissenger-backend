5c5a5b11ba3d00e3a31d2601d760ccceda121a93
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-21 18:46:42 +0600

    feat: add created at field to version

--- apps/core/models.py
+++ apps/core/models.py
@@ -6,6 +6,8 @@ class Version(models.Model):
     minor = models.PositiveSmallIntegerField(verbose_name='minor')
     patch = models.PositiveSmallIntegerField(verbose_name='patch')
 
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         verbose_name = 'version'
         verbose_name_plural = 'versions'
