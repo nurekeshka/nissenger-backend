96c74db27fedbe98a4bf19695d8e84105d8be1ee
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2022-12-21 18:44:14 +0600

    feat: add version serializer

---
+++ apps/core/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from . import models
+
+
+class VersionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Version
+        fields = ('major', 'minor', 'patch')
